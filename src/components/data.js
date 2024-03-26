42c7fb800acd85ec4425cbe8cad1596b4e45dba7
jeff45060 <34991097+jeff45060@users.noreply.github.com>
2024-03-26 08:25:27 -0400

    added data and header component

---
+++ src/components/data.js
@@ -0,0 +1,20 @@
+export const socialLinks = [
+  {
+    id: 1,
+    href: 'http://twitter.com/',
+    platform: 'twitter',
+    username: 'jeffstamey',
+  },
+  {
+    id: 2,
+    href: 'http://facebook.com/',
+    platform: 'facebook',
+    username: 'jeffstamey',
+  },
+  {
+    id: 3,
+    href: 'http://github.com/',
+    platform: 'github',
+    username: 'jeff45060',
+  },
+]
